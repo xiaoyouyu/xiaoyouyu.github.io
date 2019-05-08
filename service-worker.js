7a7f1c7deabe1b0253862417a14d330c4804b9cd
xiaoyouyu <751914807@qq.com>
2019-05-08 13:50:48 +0800

    1

---
+++ service-worker.js
@@ -0,0 +1,204 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "d3c6c6c3b7b59e46e8ec490b225ba46c"
+  },
+  {
+    "url": "app.png",
+    "revision": "daf2b980da9988091a28e0e300ba604c"
+  },
+  {
+    "url": "assets/css/0.styles.f019790a.css",
+    "revision": "5a4df30ba26765b03e79a380f796ff97"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/10.28ecd562.js",
+    "revision": "888f4eb81f1eb52efe4c687870a40319"
+  },
+  {
+    "url": "assets/js/11.6804734c.js",
+    "revision": "266ac8861b6c5a968c12da6898721b42"
+  },
+  {
+    "url": "assets/js/12.374b6c08.js",
+    "revision": "a195e7c2ebb432ebe10d78172d9f408c"
+  },
+  {
+    "url": "assets/js/13.485c557d.js",
+    "revision": "c8266df48229cddf3e4176255ca80c95"
+  },
+  {
+    "url": "assets/js/14.b9621e72.js",
+    "revision": "067efd4c6bcbedde669dea4ecb512692"
+  },
+  {
+    "url": "assets/js/15.0c842e93.js",
+    "revision": "5c30b90ace903325b9b1acf57b9e58ee"
+  },
+  {
+    "url": "assets/js/16.501cc97a.js",
+    "revision": "a788190479952dc749cd1497101b430a"
+  },
+  {
+    "url": "assets/js/2.67adf6fa.js",
+    "revision": "b30b6965aa4e35d014e3f71d30c535a3"
+  },
+  {
+    "url": "assets/js/3.c98e1996.js",
+    "revision": "2bb6e6df0754abc9e73b6b122f565611"
+  },
+  {
+    "url": "assets/js/4.32511c13.js",
+    "revision": "b87fe4a217261b00b2ab985e3d0c9196"
+  },
+  {
+    "url": "assets/js/5.d8048fa3.js",
+    "revision": "25c34fc4aa777ad998ecddf31d442cd7"
+  },
+  {
+    "url": "assets/js/6.e13555a6.js",
+    "revision": "20ac8c8c4614525d32683579dc2e2141"
+  },
+  {
+    "url": "assets/js/7.99d30589.js",
+    "revision": "e76bdf174f3be5925faa0c5f7b990c69"
+  },
+  {
+    "url": "assets/js/8.bfb5acc5.js",
+    "revision": "ab63d662e29f64db0283400f69138a65"
+  },
+  {
+    "url": "assets/js/9.40122979.js",
+    "revision": "457cd98d04cddd1928eb8e71f9aaade2"
+  },
+  {
+    "url": "assets/js/app.33eeca11.js",
+    "revision": "bc08f5677b9d07b0c816278164b27bf1"
+  },
+  {
+    "url": "docs/guide/index.html",
+    "revision": "bb8328a4db9d5d8133999b20ea47e94a"
+  },
+  {
+    "url": "docs/markdown/Emoji表情包.html",
+    "revision": "a0b2a05d8a33281d548440a71d18d675"
+  },
+  {
+    "url": "docs/markdown/index.html",
+    "revision": "3874a4d518eedda02b6db4386016f3e4"
+  },
+  {
+    "url": "docs/svn/index.html",
+    "revision": "1382f57e6951d5ed9b02b4a7aa3f304c"
+  },
+  {
+    "url": "docs/vue/index.html",
+    "revision": "6056a11e0301b3f6e857c7c19a55a02f"
+  },
+  {
+    "url": "docs/vue/vuex/index.html",
+    "revision": "7f6951d17ce6487729f79739e6c04237"
+  },
+  {
+    "url": "docs/vue/vue项目调试.html",
+    "revision": "c6ac9683383c03cf161add923afa54cb"
+  },
+  {
+    "url": "favicon-32x32.png",
+    "revision": "003a4f323ce077c0eb1c57a769d81b04"
+  },
+  {
+    "url": "icons/hbb/apple-launch-1242x2208.png",
+    "revision": "63811de6896615b9a6f77127b2751414"
+  },
+  {
+    "url": "icons/hbb/apple-launch-1536x2048.png",
+    "revision": "0f20220907814fe2582bd56855b03a07"
+  },
+  {
+    "url": "icons/hbb/apple-launch-640x1136.png",
+    "revision": "78b050616d3b55131a7c2cd4196792a4"
+  },
+  {
+    "url": "icons/hbb/apple-launch-750x1334.png",
+    "revision": "f33a4c448368585e5887b8a70e18b803"
+  },
+  {
+    "url": "icons/icon-128x128.png",
+    "revision": "160ca24b75d22ece99e69b13b9bc251d"
+  },
+  {
+    "url": "icons/icon-144x144.png",
+    "revision": "114a9280acf551005fb2de5800725c56"
+  },
+  {
+    "url": "icons/icon-152x152.png",
+    "revision": "f260f2e03536b1853e763b97ce78f998"
+  },
+  {
+    "url": "icons/icon-192x192.png",
+    "revision": "428df90a089b949e37694e985ca2ff47"
+  },
+  {
+    "url": "icons/icon-384x384.png",
+    "revision": "da14bbf9df09a0e7aca7af0f1d1abb95"
+  },
+  {
+    "url": "icons/icon-512x512.png",
+    "revision": "93917ce5dda3d75f02bc69d1c24b7faa"
+  },
+  {
+    "url": "icons/icon-72x72.png",
+    "revision": "53eec6536bef9b49242c3873f0c19612"
+  },
+  {
+    "url": "icons/icon-96x96.png",
+    "revision": "c7c8c480bec2dfd47b8ce4f1aaf9f275"
+  },
+  {
+    "url": "icons/touch-bar.svg",
+    "revision": "c4279f77fbbde00f515a9542868dba4d"
+  },
+  {
+    "url": "index.html",
+    "revision": "890248b3a0078bd1fadaeb37b4fc2ee0"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
